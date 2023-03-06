fd1edcd8e308cd317377625377c0f92327113a34
pelmeska08 <127107579+pelmeska08@users.noreply.github.com>
2023-03-06 15:44:09 +0300

    update

---
+++ proghueta18.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_prime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int a = 1;
+    int b, c;
+    cout << "Enter two numbers a and b: ";
+    cin >>  b;
+    cout << "Summ of not prime numbers " << a <<" and " << b << " are: ";
+    for (int i = a; i < b; i++) {
+        if (is_prime(i)) {
+            c = i;
+        }
+    }
+    cout << c << " ";
+    cout << endl;
+    cin.get();cin.get();
+}
