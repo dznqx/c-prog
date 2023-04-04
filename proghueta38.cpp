f628af6b110f122d1a98f14d125059ba59bdfd56
purp77 <shvd1003@mail.ru>
2023-04-04 12:56:19 +0300

    ruert

---
+++ proghueta38.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    double realPart;
+    double imaginaryPart;
+
+public:
+    Complex(double r = 0, double i = 0) {
+        realPart = r;
+        imaginaryPart = i;
+    }
+
+    void display() {
+        cout << realPart << " + " << imaginaryPart << "i" << endl;
+    }
+
+    Complex conjugate() {
+        return Complex(realPart, -imaginaryPart);
+    }
+
+    bool isEqual(Complex c) {
+        return (realPart == c.realPart && imaginaryPart == c.imaginaryPart);
+    }
+
+    Complex operator+(Complex c) {
+        return Complex(realPart + c.realPart, imaginaryPart + c.imaginaryPart);
+    }
+
+    Complex operator-(Complex c) {
+        return Complex(realPart - c.realPart, imaginaryPart - c.imaginaryPart);
+    }
+
+    Complex operator*(Complex c) {
+        return Complex(realPart * c.realPart - imaginaryPart * c.imaginaryPart,
+            realPart * c.imaginaryPart + imaginaryPart * c.realPart);
+    }
+
+    Complex operator/(Complex c) {
+        double denominator = c.realPart * c.realPart + c.imaginaryPart * c.imaginaryPart;
+        return Complex((realPart * c.realPart + imaginaryPart * c.imaginaryPart) / denominator,
+            (imaginaryPart * c.realPart - realPart * c.imaginaryPart) / denominator);
+    }
+};
+
+int main() {
+    Complex c1(2, 3);
+    Complex c2(4, -5);
+
+    cout << "c1 = ";
+    c1.display();
+
+    cout << "c2 = ";
+    c2.display();
+
+    Complex sum = c1 + c2;
+    cout << "c1 + c2 = ";
+    sum.display();
+
+    Complex diff = c1 - c2;
+    cout << "c1 - c2 = ";
+    diff.display();
+
+    Complex prod = c1 * c2;
+    cout << "c1 * c2 = ";
+    prod.display();
+
+    Complex quot = c1 / c2;
+    cout << "c1 / c2 = ";
+    quot.display();
+
+    Complex conj = c1.conjugate();
+    cout << "Conjugate of c1 = ";
+    conj.display();
+
+    bool isEqual = c1.isEqual(c2);
+    cout << "c1 and c2 are " << (isEqual ? "equal" : "not equal") << endl;
+
+    return 0;
+}
